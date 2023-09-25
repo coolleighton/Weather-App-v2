2c8ea060d858f5427f09c858d4ca8fbf015feba1
coolleighton <leighton.coughlin1@gmail.com>
2023-09-25 21:09:23 +0100

    added basic functions + a display

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // API KEY = be9b75a065e73e1cac727dcc8207ba9e //
 
 // selectors and variables //
-const city = "cardiff";
+const metric = true
+const city = "new york";
 const searchBarButton = document.querySelector("#search-bar-btn");
 const SearchBarInput = document.querySelector("#search-bar-text");
+
+const cityLocation = document.querySelector("#location")
+const currentDate = document.querySelector("#current-date")
+const temp = document.querySelector("#temp")
+const description = document.querySelector("#description")
+const feelsLike = document.querySelector("#feels-like")
+const wind = document.querySelector("#wind")
+const humidity = document.querySelector("#humidity")
+const cloudiness = document.querySelector("#cloudiness")
+const visibillity = document.querySelector("#visibillity")
+const sunrise = document.querySelector("#sunrise")
+const sunset = document.querySelector("#sunset")
+
+
 
 // search bar functions //
 searchBarButton.addEventListener("click", () => {
   if (SearchBarInput.value) {
-    console.log(SearchBarInput.value);
+    getWeatherDataByCity(SearchBarInput.value);
   } else {
     console.log("please enter a city");
   }
@@ -42,8 +57,28 @@ async function getWeatherData(lat, lon) {
   );
 
   response.json().then(function (response) {
-    console.log(response);
+    console.log(response)
+    let currentTime = new Date().toString();
+    
+    if (metric) {
+      cityLocation.textContent = response.name + ", " + response.sys.country
+      currentDate.textContent = currentTime
+      temp.textContent = Math.round(response.main.temp - 273.15) + "°C"
+      description.textContent = response.weather[0].description
+      feelsLike.textContent = "Feels like " + Math.round(response.main.feels_like - 273.15) + "°C"
+      wind.textContent = response.wind.speed + "m/s"
+      humidity.textContent = response.main.humidity + "%"
+      cloudiness.textContent = response.clouds.all + "%"
+      visibillity.textContent = (response.visibility / 1000) + "km"
+      sunrise.textContent = response.sys.sunrise.toString();
+      sunset.textContent = response.sys.sunset.toString();
+
+
+
+    }
+      
   });
 }
 
-getWeatherDataByCity(city);
+getWeatherDataByCity(city)
+
